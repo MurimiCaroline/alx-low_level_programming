4393ab7b91ae173f82b5d33baf7cf3649b8871a8
MurimiCaroline <murimicaroline20@gmail.com>
2022-09-11 13:52:02 -0700

    rectifying errors

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/*
+ *main-prints out "Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle.");
+	return (0);
+}
