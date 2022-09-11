fa23fe1ef09db990dcb61589fed5860f04bb9a06
MurimiCaroline <murimicaroline20@gmail.com>
2022-09-11 14:06:39 -0700

    tuning to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 /*
  *main-Serves as the entry point of this program
- *Return: Returns zero by default if no errors otherwise returns a non zero value
+ *Return: Returns zero by default if no errors
+ otherwise returns a non zero value
  */
- int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle.");
 	return (0);
