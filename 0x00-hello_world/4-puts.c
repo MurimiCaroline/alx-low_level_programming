06c76a693c6af14b264f4916cfdb9987bbbd8358
MurimiCaroline <murimicaroline20@gmail.com>
2022-09-11 14:03:57 -0700

    rectifying 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 /*
- *main-prints out "Programming is like building a multilingual puzzle"
+ *main-Serves as the entry point of this program
+ *Return: Returns zero by default if no errors otherwise returns a non zero value
  */
-int main(void)
+ int main (void)
 {
-	puts("Programming is like building a multilingual puzzle.");
+	puts("\"Programming is like building a multilingual puzzle.");
 	return (0);
 }
