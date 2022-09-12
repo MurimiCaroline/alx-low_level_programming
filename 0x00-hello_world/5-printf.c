7a3c925b474dd9d3eb57b3e87a6ea4f89d958c36
MurimiCaroline <murimicaroline20@gmail.com>
2022-09-12 13:56:54 -0700

    rectifying error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main-entry point for the program.
- *
- * Return0 if no errors, return nonzero if errors
+ * main-entry point for the program.
+ * Return:0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
