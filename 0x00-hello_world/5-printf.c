38db5f13f1f0184431caf3931e1b508926be1d2d
MurimiCaroline <murimicaroline20@gmail.com>
2022-09-12 06:19:48 -0700

    program that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  *main-entry point for the program.
  *
  * Return0 if no errors, return nonzero if errors
@@ -7,5 +7,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main-is the entry point of the program.
+ *
+ *Return:zero if no error. non-zero value if errors.
+ */
+int main(void)
+{
+	printf("Size of a char: %ld bytes (s)\n", sizeof(char));
+	printf("Size of an int: %ld bytes (s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes (s)\n", sizeof(longint));
+	prtintf("Sizeof a float: %ld bytes (s)\n", sizeof(float));
+	return(0);
+}
+
