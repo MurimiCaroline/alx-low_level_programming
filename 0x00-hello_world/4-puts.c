7faf1b8fe18c004c56edfeea3bda8eebb7a940d1
MurimiCaroline <murimicaroline20@gmail.com>
2022-09-12 11:50:00 -0700

    corrected error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /*
- *main-Serves as the entry point of this program
- *Return: Returns zero by default if no errors
+ * main - Serves as the entry point of this program
+ * Description: prints Programming is like building a multilingual puzzle
+ * Return : Returns zero by default if no errors
  otherwise returns a non zero value
  */
 int main(void)
