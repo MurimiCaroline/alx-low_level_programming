17c8e04d366ab15d2a75098cd251a3af142aea8c
MurimiCaroline <murimicaroline20@gmail.com>
2022-09-16 06:25:48 -0700

    when you change one, change all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,9 +21,9 @@ int main(void)
 if (n > 0)
 	printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("is zero\n", n);
+		printf("%d is zero\n", n);
 	else
-		printf("is negative\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 	}
 
