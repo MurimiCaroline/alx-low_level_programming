2b3cb312bb41234ae5cebaf3c6bef96359239ca1
MurimiCaroline <murimicaroline20@gmail.com>
2022-09-16 13:35:24 -0700

    less mistakes here

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- entry point of the program.
+ *
+ * Return:gives 0 on success.
+ */
+int main(void)
+	{
+		char l;
+
+	for (l = 'a'; l < = 'z'; l++)
+		putchar (l);
+
+		putchar('\n');
+		return (0);
+	}
